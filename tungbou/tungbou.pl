ac8fed461fc9ecccc8901798294959e338cee1cc
Madeleine <gankiann.niu@gmail.com>
2026-03-05 17:34:32 -0500

    add support for multiple parents

--- tungbou/tungbou.pl
+++ tungbou/tungbou.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+no warnings "utf8";
 
 use TOML::Tiny;
 use Getopt::Long;
@@ -24,47 +25,52 @@ if ( $dry )
 my $config_text = path($config_path)->slurp_utf8;
 my $config = from_toml $config_text;
 
-my $parent = $config->{parent};
-my @children = @{$parent->{children}};
-my $parent_dir = glob $parent->{dir};
+my @roots = @{$config->{roots}};
 
-foreach my $child ( @children )
+foreach my $parent ( @roots )
 {
-  my $child_dir = $child->{dir};
+  my @children = @{$parent->{children}};
+  my $parent_dir = $parent->{dir} =~ s/~/$ENV{HOME}/r;
+  print "$parent_dir\n";
 
-  if ( not defined $child_dir )
+  foreach my $child ( @children )
   {
-    warn "no directory configured for $child, skipping...\n";
-    next;
-  }
+    my $child_dir = $child->{dir};
+    my $host = "$child->{hostname}.local";
 
-  my $host = "$child->{hostname}.local";
-  my $command = "rsync -avhz --progress \"$parent_dir\" $host:\"$child_dir\"";
-
-  if ( $dry )
-  {
-    print "$command\n";
-  }
-  else
-  {
-    print "testing connectivity to $host...\n";
-    my $p = Net::Ping->new("tcp");
-
-    die "$host is not reachable\n"
-      unless $p->ping($host, 5);
-
-    print "$command..? [Y/n]: ";
-    my $answer = <STDIN>;
-    chomp $answer;
-
-    if ( $answer =~ /\Ano?\z/i )
+    if ( not defined $child_dir )
     {
-      warn "skipping sync for $child...\n";
+      warn "no directory configured for $host, skipping...\n";
       next;
+    }
+
+    my $command = "rsync -avhz --progress \"$parent_dir\" $host:\"$child_dir\"";
+
+    if ( $dry )
+    {
+      print "$command\n";
     }
     else
     {
-      system $command;
+      print "testing connectivity to $host...\n";
+      my $p = Net::Ping->new("tcp");
+
+      die "$host is not reachable\n"
+        unless $p->ping($host, 5);
+
+      print "$command..? [Y/n]: ";
+      my $answer = <STDIN>;
+      chomp $answer;
+
+      if ( $answer =~ /\Ano?\z/i )
+      {
+        warn "skipping sync for $host...\n";
+        next;
+      }
+      else
+      {
+        system $command;
+      }
     }
   }
 }
